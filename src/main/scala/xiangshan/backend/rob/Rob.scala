fd0051792ece289e7383af065878e99f775696e7
Li Qianruo <liqianruo20s@ict.ac.cn>
2021-12-23 14:29:19 +0800

    Various adjustment on trigger stuff
    1. Initialize regs that control triggers in memblock and frontend
    2. Select only backendhit signals to enter ExceptionGen's writeback ports

--- src/main/scala/xiangshan/backend/rob/Rob.scala
+++ src/main/scala/xiangshan/backend/rob/Rob.scala
@@ -829,7 +829,8 @@ class RobImp(outer: Rob)(implicit p: Parameters) extends LazyModuleImp(outer)
     exc_wb.bits.singleStep      := false.B
     exc_wb.bits.crossPageIPFFix := false.B
     // TODO: make trigger configurable
-    exc_wb.bits.trigger         := wb.bits.uop.cf.trigger
+    exc_wb.bits.trigger.clear()
+    exc_wb.bits.trigger.backendHit         := wb.bits.uop.cf.trigger.backendHit
     println(s"  [$i] ${configs.map(_.name)}: exception ${exceptionCases(i)}, " +
       s"flushPipe ${configs.exists(_.flushPipe)}, " +
       s"replayInst ${configs.exists(_.replayInst)}")
