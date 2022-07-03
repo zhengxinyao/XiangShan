7ebd4d17f7d4d74059cdad7878670f517f507974
LinJiawei <linjiawei20s@ict.ac.cn>
2022-07-03 11:44:33 +0800

    Reduce fmac size

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -35,7 +35,7 @@ import huancun._
 
 class BaseConfig(n: Int) extends Config((site, here, up) => {
   case XLen => 64
-  case DebugOptionsKey => DebugOptions()
+  case DebugOptionsKey => DebugOptions(AlwaysBasicDiff = false)
   case SoCParamsKey => SoCParameters()
   case PMParameKey => PMParameters()
   case XSTileKey => Seq.tabulate(n){ i => XSCoreParameters(HartId = i) }
@@ -302,8 +302,18 @@ class MediumConfig(n: Int = 1) extends Config(
 )
 
 class DefaultConfig(n: Int = 1) extends Config(
-  new WithNKBL3(6 * 1024, inclusive = false, banks = 4, ways = 6)
+  new WithNKBL3(4 * 1024, inclusive = false, banks = 4, ways = 8)
     ++ new WithNKBL2(2 * 512, inclusive = false, banks = 4, alwaysReleaseData = true)
     ++ new WithNKBL1D(128)
-    ++ new BaseConfig(n)
+    ++ new BaseConfig(n).alter((site, here, up) => {
+      case XSTileKey => up(XSTileKey).map( x => 
+        x.copy(
+          exuParameters = x.exuParameters.copy(
+            FmacCnt = 2,
+            FmiscCnt = 2,
+            FmiscDivSqrtCnt = 0,
+          )
+        )
+      )
+    })
 )
