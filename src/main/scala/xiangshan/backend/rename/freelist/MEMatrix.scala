7c453f03241665f952e830ad93fc1f1b9f20f8c5
Haojin Tang <tanghaojin@outlook.com>
2023-03-13 20:11:34 +0800

    MEMatrix: limit commit of mv inst

--- src/main/scala/xiangshan/backend/rename/freelist/MEMatrix.scala
+++ src/main/scala/xiangshan/backend/rename/freelist/MEMatrix.scala
@@ -24,22 +24,22 @@ import xiangshan._
 class METable extends Bundle {
   val table = MixedVec(Seq.tabulate(30)(i => Vec(30 - i, Bool())))
 
-  def getBit(row: Int, col: Int): Bool =
+  def getBit(row: Int, col: Int, isWrite: Boolean): Bool =
     if (row == 0 || col == 0 || row == col)
-      WireDefault(false.B)
+      if (isWrite) WireDefault(false.B) else false.B
     else if (col > row)
       table(row - 1)(col - row - 1)
     else
-      getBit(col, row)
+      getBit(col, row, isWrite)
 
   def read(addr: UInt): UInt =
-    Mux1H(UIntToOH(addr), Seq.tabulate(32)(i => VecInit.tabulate(32)(j => getBit(i, j)).asUInt))
+    Mux1H(addr, Seq.tabulate(32)(i => VecInit.tabulate(32)(j => getBit(i, j, false)).asUInt))
 
   def write(addr: UInt, data: UInt): Unit =
-    for (i <- 0 until 32)
-      when(addr === i.U) {
-        for (j <- 0 until 32)
-          getBit(i, j) := data(j)
+    for (i <- 1 until 32)
+      when(addr(i)) {
+        for (j <- 1 until 32)
+          getBit(i, j, true) := data(j)
       }
 }
 
@@ -51,28 +51,37 @@ class MEMatrix(implicit p: Parameters) extends XSModule {
 
   val table = RegInit(0.U.asTypeOf(new METable))
   val tableNext = WireDefault(VecInit.fill(CommitWidth + 1)(table))
-  val commit = RegNext(io.commit, 0.U.asTypeOf(io.commit))
-  val isMove = commit.info.map(_.isMove)
-  val isVset = commit.info.map(_ => false.B) // commit.info.map(_.ldest(5)) // ldest == 32
+  val isMove = io.commit.info.map(x => RegNext(x.isMove))
+  val isVset = io.commit.info.map(x => false.B) // commit.info.map(_.ldest(5)) // ldest == 32
+  val lsrcOH = io.commit.info.map(x => RegNext(UIntToOH(x.lsrc)))
+  val ldestOH = io.commit.info.map(x => RegNext(UIntToOH(x.ldest(4, 0))))
+  val old_pdest = io.commit.info.map(x => RegNext(x.old_pdest))
 
   val freeValid = WireDefault(VecInit.fill(CommitWidth)(false.B))
   for (i <- 0 until CommitWidth) {
-    val ldstArray = tableNext(i).read(commit.info(i).ldest(4, 0))
-    val lsrcArray = tableNext(i).read(commit.info(i).lsrc)
-    val maskArray = UIntToOH(commit.info(i).lsrc)
-    val isSelfMove = isMove(i) && commit.info(i).ldest(4, 0) === commit.info(i).lsrc
+    val ldstArray = tableNext(i).read(ldestOH(i))
+    val lsrcArray = tableNext(i).read(lsrcOH(i))
+    val isSelfMove = RegNext(io.commit.info(i).isMove && io.commit.info(i).ldest(4, 0) === io.commit.info(i).lsrc)
+    val do_write = RegNext(io.commit.isCommit && io.commit.commitValid(i) && io.commit.info(i).rfWen, false.B)
     tableNext(i + 1) := tableNext(i)
-    when(commit.isCommit && commit.commitValid(i) && commit.info(i).rfWen) {
-      // safe for vset as the lower 5 bits are 0s
-      tableNext(i + 1).write(commit.info(i).ldest(4, 0), (lsrcArray | maskArray) & VecInit.fill(32)(isMove(i)).asUInt)
-      // preg 0 should never be freed
-      // vset always frees old_pdest which is not preg 0
-      when(!isSelfMove && (!ldstArray.orR || isVset(i)) && commit.info(i).old_pdest =/= 0.U) {
-        freeValid(i) := true.B
+    when(do_write) {
+      if (i >= 3) {
+        tableNext(i + 1).write(ldestOH(i), 0.U(32.W))
+        when((!ldstArray.orR || isVset(i))) {
+          freeValid(i) := true.B
+        }
+      } else {
+        // safe for vset as the lower 5 bits are 0s
+        tableNext(i + 1).write(ldestOH(i), (lsrcArray | lsrcOH(i)) & VecInit.fill(32)(isMove(i)).asUInt)
+        // preg 0 should never be freed
+        // vset always frees old_pdest which is not preg 0
+        when(!isSelfMove && (!ldstArray.orR || isVset(i))) {
+          freeValid(i) := true.B
+        }
       }
     }
-    io.freeRegs(i).valid := RegNext(freeValid(i), false.B)
-    io.freeRegs(i).bits := RegNext(commit.info(i).old_pdest)
+    io.freeRegs(i).valid := RegNext(freeValid(i), false.B) && RegNext(old_pdest(i) =/= 0.U)
+    io.freeRegs(i).bits := RegNext(old_pdest(i))
   }
   table := tableNext.last
 }
